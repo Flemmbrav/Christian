ecc17d410bb888e5b4f73acc40c47f0a488f0987
Flemmbrav <nshplus@gmail.com>
2021-04-15 20:45:22 +0200

    init

---
+++ Compile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo 'Havingh fun with some things uwu'
+echo -e '======================================================\n'
+
+echo 'This bash compiles this repository into a new folder'
+echo -e 'called compiled, makeobj must be in root folder.\n'
